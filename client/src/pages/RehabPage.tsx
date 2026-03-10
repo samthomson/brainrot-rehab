bc2d80751e59c2a37c6a492989029b3a2b24b2b5
sam <samt@samt.st>
2026-03-10 20:15:35 +0700

    column

--- client/src/pages/RehabPage.tsx
+++ client/src/pages/RehabPage.tsx
@@ -201,7 +201,7 @@ export default function RehabPage() {
   };
 
   return (
-    <div className="max-w-[1600px] mx-auto p-6 space-y-6">
+    <div className="max-w-5xl mx-auto p-6 space-y-6">
         <div className="flex items-end justify-between pb-6">
             <div>
               <h1 className="text-4xl font-black tracking-tight">Rehab</h1>
